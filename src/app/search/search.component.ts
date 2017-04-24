8f2cc6dc88df80bc3020ca7ad399e5cdcbab036d
Waled ABU SHURAITAH <waled.abushuraitah-ext@veolia.com>
2017-04-24 17:53:37 +0200

    Don’t hit me twice

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {WikipediaService} from "../wikipedia.service";
 import {FormControl} from "@angular/forms";
 import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/distinctUntilChanged";
 
 
 @Component({
@@ -13,7 +14,7 @@ export class SearchComponent implements OnInit {
   items:Array<string>;
   term = new FormControl();
   constructor(private wikipediaService: WikipediaService) {
-    this.term.valueChanges.debounceTime(400).subscribe( term => this.wikipediaService.search(term).then(items => { console.log(term);this.items = items } ));
+    this.term.valueChanges.debounceTime(400).distinctUntilChanged().subscribe( term => this.wikipediaService.search(term).then(items => { console.log(term);this.items = items } ));
   }
 
   ngOnInit() {
